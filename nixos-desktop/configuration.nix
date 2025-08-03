25dc243f1a2bda0fea6687cf678725619cae5a59
Marc Miller <marcjmiller@outlook.com>
2025-08-03 15:23:06 -0400

    fix: remove gnome

--- nixos-desktop/configuration.nix
+++ nixos-desktop/configuration.nix
@@ -51,7 +51,6 @@ in
 
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
